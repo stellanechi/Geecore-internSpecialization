4d01dd3e46c52b59e3435a8b32443a4f9ea2e5c3
stellanechi <stellanechi65@gmail.com>
2023-08-15 10:37:10 +0100

    updated

---
+++ example.js
@@ -0,0 +1,8 @@
+document.getElementById('btn').style.backgroundColor = 'red';
+document.getElementById('btn').style.border = 'none';
+
+document.getElementsByTagName('input')[0].style.padding = '5px 10px';
+
+document.getElementsByClassName('input')[0].style.backgroundColor = 'cyan';
+
+
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+
+
+let anime = document.getElementsByClassName ('hello');
+
 function message() {
-    document.getElementById('message')
+    anime.style.display ='none';
 }
+
+// document.getElementsByClassName ("alert")[0].style.backgroundColor = 'blue';
+// alert('alert')
+
+//  const message =  document.getElementsByClassName ('hello');
+// alert('hello')
+
+// const full =  document.querySelectorAll ('div');
+// (message)
